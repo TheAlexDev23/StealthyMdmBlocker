22bdc40aa295381c2dadd172ac138c7ac25cc510
TheAlexDev23 <thealexdev23@gmail.com>
2024-01-17 17:51:31 +0100

    refactor: add pyright configuration and rewrite some scripts to please it

--- ProfileModification/MDMProfileManager.py
+++ ProfileModification/MDMProfileManager.py
@@ -1,16 +1,22 @@
 import XMLHelpers
 
+
 def get_version(xml: str) -> str:
     return XMLHelpers.get_value_pair(xml, "PayloadDisplayName", "string", "key")
 
+
 def append_allowed_apps(xml: str, allowed_apps: list) -> str:
-    current_allowed_apps = XMLHelpers.get_value_pair(xml, "allowListedAppBundleIDs", "array", "key")
-    
+    current_allowed_apps = XMLHelpers.get_value_pair(
+        xml, "allowListedAppBundleIDs", "array", "key"
+    )
+
     for app_bundle_id in allowed_apps:
         if app_bundle_id not in current_allowed_apps:
             current_allowed_apps += "\n" + f"<string>{app_bundle_id}</string>"
 
-    xml = XMLHelpers.update_value_pair(xml, "allowListedAppBundleIDs", current_allowed_apps, "array", "key")
+    xml = XMLHelpers.update_value_pair(
+        xml, "allowListedAppBundleIDs", current_allowed_apps, "array", "key"
+    )
 
     return xml
 
@@ -26,12 +32,18 @@ def update_restrictions(xml: str, restriction_modifications: dict) -> str:
 
     return xml
 
+
 def update_web_filters(xml: str) -> str:
     xml = XMLHelpers.update_boolean_property(xml, "AutoFilterEnabled", False)
-    
+
     # Idk if making the array empty will break shit, so i give it at least 2 elements just in case
     # Nobody should suffer from these restrictions, because nobody uses bing and omegle is shut down
-    updated_list = "<string>https://www.bing.com</string>\n<string>https://www.omegle.com</string>"
-    xml = XMLHelpers.update_value_pair(xml, "DenyListURLs", updated_list, "array", "key")
-    
+    updated_list = (
+        "<string>https://www.bing.com</string>\n<string>https://www.omegle.com</string>"
+    )
+    xml = XMLHelpers.update_value_pair(
+        xml, "DenyListURLs", updated_list, "array", "key"
+    )
+
     return xml
+
--- ProfileModification/MitmproxyMDMProfileModifierEndpoint.py
+++ ProfileModification/MitmproxyMDMProfileModifierEndpoint.py
@@ -1,13 +1,10 @@
 import base64
-import sys
 
 from mitmproxy import http
 
 import CommandXMLParser
 import XMLHelpers
 import MDMProfileManager
-
-sys.path.insert(0, "../Helpers")
 
 from Logger import Logger
 
@@ -21,33 +18,29 @@ allowed_app_bundle_ids = [
     "com.burbn.instagram",
     "com.atebits.Tweetie2",
     "ch.protonmail.protonmail",
-    "ch.protonmail.drive"
+    "ch.protonmail.drive",
 ]
 
 restriction_modifications = {
     # This one is not found in the restrictions category but due to the way the parser works it'll be fine.
-    "PayloadRemovalDisallowed"     : "false",
-    
-    # restrictions category 
-    "allowAppInstallation"         : "true",
-    "allowAppRemoval"              : "true",
-    "allowInAppPurchases"          : "true",
-    "allowAssistant"               : "true",
-    "allowAirDrop"                 : "true",
-    "allowItunes"                  : "true",
-    "allowSafari"                  : "true",
-    "forceWifiPowerOn"             : "false",
-    "allowVPNCreation"             : "true",
-    "allowEraseContentAndSettings" : "true",
-    "allowDeviceNameModification"  : "true",
-    "allowUIAppInstallation"       : "true"
+    "PayloadRemovalDisallowed": "false",
+    # restrictions category
+    "allowAppInstallation": "true",
+    "allowAppRemoval": "true",
+    "allowInAppPurchases": "true",
+    "allowAssistant": "true",
+    "allowAirDrop": "true",
+    "allowItunes": "true",
+    "allowSafari": "true",
+    "forceWifiPowerOn": "false",
+    "allowVPNCreation": "true",
+    "allowEraseContentAndSettings": "true",
+    "allowDeviceNameModification": "true",
+    "allowUIAppInstallation": "true",
 }
 
-# Logging configuration
-use_email_logging = False
-verbose: bool = "-v" in sys.argv
-
 logger = Logger()
+
 
 def patch_mdm_configuration(request_xml: str) -> str:
     target_most_recent_version = False
@@ -57,33 +50,52 @@ def patch_mdm_configuration(request_xml: str) -> str:
     mdm_xml = base64.b64decode(encoded_conf.encode("utf-8")).decode("utf-8")
 
     # The server for some fucking reason sends 2 different mdm configurations.
-    # One of them seems to be for younger classes or straight up outdated. 
+    # One of them seems to be for younger classes or straight up outdated.
     # This is a hardcoded check since the mdm provider has no fucking versioning rules and the one that seems to be the active has 23 in its display name.
     # However it doesn't really matter as i dont really know which profile is used so patching both should not cause any damage.
-    if target_most_recent_version and "23" not in MDMProfileManager.get_version(mdm_xml):
-        logger.log("Will not apply patch. Not most recent version.", f"Decrypted mdm config:\n{mdm_xml}\n\Whole response:\n{request_xml}")
-        return
-    
-    # Fuck python honestly. Why isn't a string a reference type? 
+    if target_most_recent_version and "23" not in MDMProfileManager.get_version(
+        mdm_xml
+    ):
+        logger.log(
+            "Will not apply patch. Not most recent version.",
+            f"Decrypted mdm config:\n{mdm_xml}\nWhole response:\n{request_xml}",
+        )
+
+        return request_xml
+
+    # Fuck python honestly. Why isn't a string a reference type?
     mdm_xml = MDMProfileManager.append_allowed_apps(mdm_xml, allowed_app_bundle_ids)
     mdm_xml = MDMProfileManager.update_restrictions(mdm_xml, restriction_modifications)
     mdm_xml = MDMProfileManager.update_web_filters(mdm_xml)
 
     encoded_conf = base64.b64encode(mdm_xml.encode("utf-8")).decode("utf-8")
 
-    request_xml = XMLHelpers.update_value_pair(request_xml, "Payload", encoded_conf, "data", "key")
+    request_xml = XMLHelpers.update_value_pair(
+        request_xml, "Payload", encoded_conf, "data", "key"
+    )
 
-    logger.log("Mdm Patch Applied", f"MDM config:\n{mdm_xml}\nWhole response:\n{request_xml}")
+    logger.log(
+        "Mdm Patch Applied", f"MDM config:\n{mdm_xml}\nWhole response:\n{request_xml}"
+    )
 
     return request_xml
 
 
 # mitmproxy entry
 def response(flow: http.HTTPFlow) -> None:
-    if "jamfcloud" not in flow.request.pretty_url: 
+    if "jamfcloud" not in flow.request.pretty_url:
         return
 
-    request_xml = XMLHelpers.sanitize(flow.response.get_text())
+    # Should not happen, but to please pyright
+    if flow.response is None:
+        return
+
+    response_text = flow.response.get_text()
+
+    if response_text is None:
+        return
+
+    request_xml = XMLHelpers.sanitize(response_text)
 
     command_type = CommandXMLParser.get_command_type(request_xml)
 
@@ -91,7 +103,9 @@ def response(flow: http.HTTPFlow) -> None:
     if command_type == CommandXMLParser.CommandType.InstallProfile:
         request_xml = patch_mdm_configuration(request_xml)
     else:
-        logger.log("Skipping command, unsuported type. ", f"Request xml:\n{request_xml}")
+        logger.log(
+            "Skipping command, unsuported type. ", f"Request xml:\n{request_xml}"
+        )
 
     flow.response.text = request_xml
 
@@ -101,6 +115,11 @@ def request(flow: http.HTTPFlow) -> None:
     if "jamfcloud" not in flow.request.pretty_url:
         return
 
-    request_xml = XMLHelpers.sanitize(flow.response.get_text())
+    request_text = flow.request.get_text()
+
+    if request_text is None:
+        return
+
+    request_xml = XMLHelpers.sanitize(request_text)
 
     logger.log("Request", f"{request_xml}")
