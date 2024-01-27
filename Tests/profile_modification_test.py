47c42f530f680bb8aee7ef84dda4af7fad496b93
TheAlexDev23 <thealexdev23@gmail.com>
2024-01-27 15:05:45 +0100

    refactor: migrate unittest to pytest

--- Tests/profile_modification_test.py
+++ Tests/profile_modification_test.py
@@ -1,13 +1,12 @@
-import unittest
+import pytest
 
 import test_helpers
 
 import sys
 
 sys.path.append(test_helpers.get_profile_modification_path())
+# MDMProfileManager imports XMLHelpers and other helpers
 sys.path.append(test_helpers.get_helpers_path())
-
-import Config
 
 import MDMProfileManager
 
@@ -20,64 +19,66 @@ V23_IN = "plain_" + V23 + ".txt"
 V225_IN = "plain_" + V225 + ".txt"
 
 
-class MdmProfileManagerTestCase(unittest.TestCase):
-    def test_version_is_latest(self):
-        self.assert_version_is_latest(V23_IN, True)
-        self.assert_version_is_latest(V225_IN, False)
-
-    def test_append_allowed_apps(self):
-        def target(input):
-            return MDMProfileManager.append_allowed_apps(
-                input, test_helpers.allowed_app_bundle_ids
-            )
-
-        self.assert_output_equals("append_allowed_apps_", target)
-
-    def test_update_restrictions(self):
-        def target(input):
-            return MDMProfileManager.update_restrictions(
-                input, test_helpers.restriction_modifications
-            )
-
-        self.assert_output_equals("update_restrictions_", target)
-
-    def test_update_web_filters(self):
-        def target(input):
-            return MDMProfileManager.update_web_filters(input)
-
-        self.assert_output_equals("update_web_filters_", target)
-
-    def test_remove_allowed_apps(self):
-        def target(input):
-            return MDMProfileManager.remove_allowed_apps(input)
-
-        self.assert_output_equals("remove_allowed_apps_", target)
-
-    def test_remove_restrictions(self):
-        def target(input):
-            return MDMProfileManager.remove_restrictions(input)
-
-        self.assert_output_equals("remove_restrictions_", target)
-
-    def assert_version_is_latest(self, input_path, match):
-        with open(TEST_FILES_DIR + input_path, "r") as fp:
-            input = fp.read()
-
-        self.assertTrue(MDMProfileManager.version_is_latest(input) == match)
-
-    def assert_output_equals(self, match_path, callback):
-        with open(TEST_FILES_DIR + V23_IN, "r") as fp:
-            input1 = fp.read()
-        with open(TEST_FILES_DIR + V225_IN, "r") as fp:
-            input2 = fp.read()
-        with open(TEST_FILES_DIR + match_path + V23 + ".txt", "r") as fp:
-            match1 = fp.read()
-        with open(TEST_FILES_DIR + match_path + V225 + ".txt", "r") as fp:
-            match2 = fp.read()
-
-        self.assertTrue(callback(input1) == match1)
-        self.assertTrue(callback(input2) == match2)
+def test_version_is_latest():
+    assert_version_is_latest(V23_IN, True)
+    assert_version_is_latest(V225_IN, False)
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_append_allowed_apps():
+    def target(input):
+        return MDMProfileManager.append_allowed_apps(
+            input, test_helpers.allowed_app_bundle_ids
+        )
+
+    assert_output_equals("append_allowed_apps_", target)
+
+
+def test_update_restrictions():
+    def target(input):
+        return MDMProfileManager.update_restrictions(
+            input, test_helpers.restriction_modifications
+        )
+
+    assert_output_equals("update_restrictions_", target)
+
+
+def test_update_web_filters():
+    def target(input):
+        return MDMProfileManager.update_web_filters(input)
+
+    assert_output_equals("update_web_filters_", target)
+
+
+def test_remove_allowed_apps():
+    def target(input):
+        return MDMProfileManager.remove_allowed_apps(input)
+
+    assert_output_equals("remove_allowed_apps_", target)
+
+
+def test_remove_restrictions():
+    def target(input):
+        return MDMProfileManager.remove_restrictions(input)
+
+    assert_output_equals("remove_restrictions_", target)
+
+
+def assert_version_is_latest(input_path, match):
+    with open(TEST_FILES_DIR + input_path, "r") as fp:
+        input = fp.read()
+
+    assert MDMProfileManager.version_is_latest(input) == match
+
+
+def assert_output_equals(match_path, callback):
+    with open(TEST_FILES_DIR + V23_IN, "r") as fp:
+        input1 = fp.read()
+    with open(TEST_FILES_DIR + V225_IN, "r") as fp:
+        input2 = fp.read()
+    with open(TEST_FILES_DIR + match_path + V23 + ".txt", "r") as fp:
+        match1 = fp.read()
+    with open(TEST_FILES_DIR + match_path + V225 + ".txt", "r") as fp:
+        match2 = fp.read()
+
+    assert callback(input1) == match1
+    assert callback(input2) == match2
